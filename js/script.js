8827071119eb0bab1be4a5782a7a9eec6af3c659
towhid0845 <towhid0845@gmail.com>
2023-11-27 12:19:00 +0600

    news and media section done

---
+++ js/script.js
@@ -0,0 +1,106 @@
+$(function () {
+  "use strict";
+
+  // fixed menu js
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('.sticky-top').addClass('shadow-lg').css('top', '0px');
+    } else {
+      $('.sticky-top').removeClass('shadow-lg').css('top', '-100px');
+    }
+  });
+
+  // mobile menu js
+  $('.mobile-topbar .icon i').click(function () {
+    $('.mobile-menu-main').addClass('show-mobile-menu')
+  })
+  $('.close-m-menu').click(function () {
+    $('.mobile-menu-main').removeClass('show-mobile-menu')
+  })
+  $('.mobile-topbar .icon i').click(function () {
+    $('.mobile-menu-overlay').addClass('show-mobile-menu-overlay')
+  })
+  $('.mobile-topbar .icon i').click(function () {
+    $('.mobile-menu-overlay').addClass('show-mobile-menu-overlay')
+  })
+  $('.close-m-menu').click(function () {
+    $('.mobile-menu-overlay').removeClass('show-mobile-menu-overlay')
+  })
+  $('.mobile-menu-overlay').click(function () {
+    $('.mobile-menu-overlay').removeClass('show-mobile-menu-overlay')
+  })
+  $('.mobile-menu-overlay').click(function () {
+    $('.mobile-menu-main').removeClass('show-mobile-menu')
+  })
+
+  $('.sub-menu ul').hide();
+  $(".sub-menu a").click(function () {
+    $(this).parent(".sub-menu").children("ul").slideToggle("100");
+    $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+  });
+
+
+  // back to top js
+  let btn = $('#button');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, '300');
+  });
+
+  // venobox js
+  new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane'
+  });
+
+  new VenoBox({
+    selector: '.my-video-links',
+  });
+
+
+    // gallery popup js
+    $('.parent-container').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery: {
+        enabled: true
+      },
+      removalDelay: 300,
+      mainClass: 'mfp-fade',
+    });
+  
+  
+    // video gallery popup js
+    $('.vidplay').magnificPopup({
+      type: 'iframe',
+      iframe: {
+        markup: '<div class="mfp-iframe-scaler">' +
+          '<div class="mfp-close"></div>' +
+          '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+          '</div>',
+        patterns: {
+          youtube: {
+            index: 'youtube.com/',
+            id: 'v=',
+            src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+          },
+        },
+        srcAction: 'iframe_src',
+      }
+    });
+
+})
